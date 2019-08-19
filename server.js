61f426a3db7e3bcf107dbd84bfc601980939b689
scottbisese <bisese.scott@gmail.com>
2019-08-19 16:43:31 -0500

    improvements

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+//localhost:3000/location?location=blah
+app.get('/location', (request,response) => {        //server route
+    try{
+        const geoData = require('./data/geo.json');
+        const location = new Location(request.query.location, geoData)
+        // throw new Error();
+        response.send(location);
+       
+    } catch(error){
+        response.status(500).send('sorry something went wrong.')
+    }
+});
+
+function Location(query, geoData) {
+    this.search_query = query;
+    this.formatted_query = geoData.results[0].formatted_address;
+    this.latitude = geoData.results[0].geometry.location.lat;
+    this.longitude = geoData.results[0].geometry.location.lng;
+}
+
+app.get('/weather',(request,response) => {
+    
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('server is listening');
+});
